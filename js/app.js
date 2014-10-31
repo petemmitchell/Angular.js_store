9eca372f230996f7131788aab50432564525b607
Pete Mitchell <petemmitchell@gmail.com>
2014-10-30 20:37:10 -0700

    This is actually levels 3 & 4. I forgot to switch branches after level 3. My bad. Everything is finally working smoothly. I used the gem 'serve' to be able to see my index.html with the templates being pulled in properly.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,73 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+
+  app.directive("productGallery", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-gallery.html",
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber) {
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: "gallery"
+    };
+  });
+
+  app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('TabController', function(){
-    this.tab = 1;
+  app.controller("ReviewController", function(){
 
-    this.setTab = function(newValue){
-      this.tab = newValue;
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
     };
 
-    this.isSet = function(tabName){
-      return this.tab === tabName;
+  });
+
+  app.directive("productDescriptions", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
     };
   });
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "product-specs.html"
+    };
+  });
+
+  app.directive("productTabs", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-tabs.html",
+      controller: function() {
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: "tab"
     };
   });
 
@@ -34,23 +81,20 @@
       color: '#CCC',
       faces: 14,
       images: [
-        "images/gem-02.gif",
-        "images/gem-05.gif",
-        "images/gem-09.gif"
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-05.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-09.gif"
       ],
       reviews: [{
         stars: 5,
         body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
+        author: "joe@example.org"
       }, {
         stars: 1,
         body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
+        author: "tim@example.org"
       }]
-    },
-    {
+    }, {
       name: 'Bloodstone',
       description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
       shine: 9,
@@ -59,51 +103,45 @@
       color: '#EEE',
       faces: 12,
       images: [
-        "images/gem-01.gif",
-        "images/gem-03.gif",
-        "images/gem-04.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif",
+        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-04.gif"
       ],
       reviews: [{
         stars: 3,
         body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
+        author: "JimmyDean@example.org"
       }, {
         stars: 4,
         body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
+        author: "gemsRock@example.org"
       }]
-    },
-    {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "images/gem-06.gif",
-        "images/gem-07.gif",
-        "images/gem-09.gif"
-      ],
-      reviews: [{
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
       }, {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }]
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif",
+          "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-07.gif",
+          "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-08.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
     }
   ];
 })();
